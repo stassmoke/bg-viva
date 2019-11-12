713f2e6cb4eb0f69ef92081ad246909f0517fa6b
Yaroslav Ilnickiy <eyarij@gmail.com>
2019-11-12 13:09:29 +0200

    fix problems and add persons

---
+++ app/Models/LegalPerson.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class LegalPerson
+ * @package App\Models
+ * @property int id
+ * @property int legal_entity_id
+ * @property string description
+ */
+class LegalPerson extends Model
+{
+    public const CREATED_AT = null;
+    public const UPDATED_AT = null;
+}
