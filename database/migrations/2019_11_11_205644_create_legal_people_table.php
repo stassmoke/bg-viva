952066cda5763851b570b6e761a3b5ed94a9cfb9
Yaroslav Ilnickiy <eyarij@gmail.com>
2019-11-12 13:12:45 +0200

    fix problems and add persons

---
+++ database/migrations/2019_11_11_205644_create_legal_people_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLegalPeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('legal_people', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('legal_entity_id')->unsigned();
+            $table->mediumText('description');
+
+            $table->index('legal_entity_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('legal_people');
+    }
+}
